8aca33ecb30bf8e96cc9d11af7bb8cf19c9aa588
Shahib <163203412+Shahibullah@users.noreply.github.com>
2025-05-28 03:33:54 +0600

    Add files via upload

---
+++ Object Creation.js
@@ -0,0 +1,10 @@
+//Object Creation 
+const person = {
+                name : "Shahib",
+                age  : 23,
+                StudentID : 22301025,
+                hobbies : ["Gaming","Music","Films"]
+            };
+console.log(person);
+console.log(typeof(person));
+console.log(person.StudentID);
