d365b81dd42732db732042630944a79413e33d68
Shahib <163203412+Shahibullah@users.noreply.github.com>
2025-05-28 03:11:57 +0600

    Add files via upload

---
+++ For Loop with Condition.js
@@ -0,0 +1,14 @@
+// For - LOOP
+my_list  = ["Pen",10,"Paper",35,"Glass",250];
+for (i = 0; i < my_list.length;i++)
+{
+    if (i % 2 == 0)
+        {console.log(my_list[i]);}
+    else 
+        {console.log(`${my_list[i]} Taka `);}
+
+        
+
+
+}
+console.log("-------------")
