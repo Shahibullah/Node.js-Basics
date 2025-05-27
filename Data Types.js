a4a99934b443516e111cfc6286de3c85de4c9e61
Shahib <163203412+Shahibullah@users.noreply.github.com>
2025-05-28 02:47:09 +0600

    Add files via upload

---
+++ Data Types.js
@@ -0,0 +1,25 @@
+/*Check Data Type*/
+let new_list = ["Shahib",22301025,(10,20)];
+//if Array or List 
+console.log(Array.isArray(new_list)+" that is an array");
+console.log(typeof(new_list));
+let number = 10.00;
+console.log(typeof(number));
+let name = "Shahib";
+console.log(typeof(name));
+let bool = true ;
+console.log(typeof(bool));
+//Big Int is use for storing large integer value
+let bigint = 1234n ;
+console.log(typeof(bigint));
+//undefined
+let x  ;
+console.log(typeof(x));
+
+/*                     Convertion                        */
+// String to Number//
+let a = "12";
+let b = Number(a) + 8;
+console.log(b);
+symbo = Symbol();
+console.log(typeof Symbol())
