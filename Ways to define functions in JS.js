ea20ce941cbeb1e3f9d72b950c94894242230346
Shahib <163203412+Shahibullah@users.noreply.github.com>
2025-05-30 03:42:47 +0600

    Add files via upload
    Three ways are shown here..

---
+++ Ways to define functions in JS.js
@@ -0,0 +1,19 @@
+console.log("server file is running");
+function  add(a,b)
+{return a +b;}
+let sum = add(10,90);
+console.log(sum);
+
+
+
+
+///////////////////////////////////Best Way///////////////////////////////////////////
+let addition = (a,b) => a + b;
+answer = addition(10,20);
+console.log(answer);
+///////////////////////////////////Best Way///////////////////////////////////////////
+
+let Summation = function (a,b,c){
+    return a + b + c;
+}
+console.log(Summation("I ","am ",'Shahib.'));
